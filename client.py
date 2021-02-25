6377fb1b7c5b2a093de30d4c0d52c3a7b6fdf64b
Wallefy Otaviani <wallefymatheus@gmail.com>
2021-02-25 19:15:30 -0300

    Modificação Inicial

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 import socket
+import sys
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+if ':' in HOST :
+    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+else :
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((HOST, PORT))
+message = 1
+s.sendall( message.to_bytes(2, byteorder='big') )
+data = s.recv(1024)
 
 print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,18 +1,46 @@
 #!/usr/bin/env python3
 
 import socket
+import sys
+import _thread
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+# HOST_IPV4 = '127.0.0.1'
+# HOST_IPV6 = '::1'
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+HOST = '::1'
+PORT = int(sys.argv[1])
+
+s = socket.create_server(HOST, *, family=AF_INET6, backlog=None, reuse_port=False, dualstack_ipv6=True)
+
+# s_tcp4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s_tcp6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+
+# s_tcp4.bind((HOST_IPV4, PORT))
+# s_tcp4.listen()
+#
+# s_tcp6.bind((HOST_IPV6, PORT,0,0))
+# s_tcp6.listen()
+
+def multi_thread(conn,s_tcp) :
     with conn:
         print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+        data = conn.recv(1024)
+        if int.from_bytes(data, byteorder='big') == 1 :
+            message = 2
+            conn.sendall(message.to_bytes(2, byteorder='big'))
+            if ':' in addr[1] :
+                s_udp = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+            else :
+                s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else :
+            print('[LOG] Erro no recebimento da mensagem de HELLO')
+
+def accept_tcp6(s, null) :
+    print("OI")
+    while True:
+        conn, addr = s.accept()
+        _thread.start_new_thread(multi_thread, (conn,s))
+
+
+_thread.start_new_thread(accept_tcp4, (s,' '))
+print('Cabo')
