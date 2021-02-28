dafed3b3163fca8aa0c073314e82f1ed30dd13c6
Matheus <wallefymatheus@gmail.com>
2021-02-28 12:18:21 -0300

    Implementação do ping pong e iniciação de transferência de arquivo

--- client.py
+++ client.py
@@ -2,9 +2,13 @@
 
 import socket
 import sys
+import os
+
+BUFFER_SIZE = 1000
 
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
+filename = sys.argv[3]
 
 if ':' in HOST :
     s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
@@ -15,5 +19,40 @@ s.connect((HOST, PORT))
 message = 1
 s.sendall( message.to_bytes(2, byteorder='big') )
 data = s.recv(1024)
+message_received_type = int.from_bytes(data[0:2], byteorder='big')
+message_received_port = int.from_bytes(data[2:6], byteorder='big')
+print('Received', message_received_type,message_received_port)
 
-print('Received', repr(data))
+if ':' in HOST :
+    s_udp = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+else :
+    s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s_udp.bind((HOST,message_received_port))
+
+if len(filename) < 16 and filename[len(filename)-4] == '.' and filename.count('.') == 1 :
+    message = (3).to_bytes(2, byteorder='big')
+    filesize = os.path.getsize(filename)
+    print(filesize)
+
+    sys.exit()
+
+    message = message + filename.encode()
+    s.sendall( message )
+    data = s.recv(1024)
+    message_received_ok = int.from_bytes(data, byteorder='big')
+    if message_received_ok == 4 :
+        print('Received', message_received_ok)
+        f = open(filename, "rb")
+
+        i = 0
+        while True :
+            message = (6).to_bytes(2, byteorder='big')
+            payload = f.read(BUFFER_SIZE)
+            if len(payload) == 0 :
+                break
+            else :
+                message = message + i.to_bytes(4, byteorder='big') + len(payload).to_bytes(2, byteorder='big') + payload
+                s_udp.sendall( message )
+                i += 1
+else :
+    print("Nome não permitido")
--- server.py
+++ server.py
@@ -4,43 +4,46 @@ import socket
 import sys
 import _thread
 
-# HOST_IPV4 = '127.0.0.1'
-# HOST_IPV6 = '::1'
+BUFFER_SIZE = 1000
 
 HOST = '::1'
 PORT = int(sys.argv[1])
 
-s = socket.create_server(HOST, *, family=AF_INET6, backlog=None, reuse_port=False, dualstack_ipv6=True)
+s = socket.create_server((HOST,PORT), family=socket.AF_INET6, dualstack_ipv6=True)
 
-# s_tcp4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s_tcp6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-
-# s_tcp4.bind((HOST_IPV4, PORT))
-# s_tcp4.listen()
-#
-# s_tcp6.bind((HOST_IPV6, PORT,0,0))
-# s_tcp6.listen()
-
-def multi_thread(conn,s_tcp) :
-    with conn:
-        print('Connected by', addr)
-        data = conn.recv(1024)
-        if int.from_bytes(data, byteorder='big') == 1 :
-            message = 2
-            conn.sendall(message.to_bytes(2, byteorder='big'))
-            if ':' in addr[1] :
-                s_udp = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-            else :
-                s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def multi_thread(conn,s) :
+    print('Connected by', addr)
+    data = conn.recv(1024)
+    if int.from_bytes(data, byteorder='big') == 1 :
+        message = (2).to_bytes(2,byteorder='big')
+        if ':' in addr[0] :
+            s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
         else :
-            print('[LOG] Erro no recebimento da mensagem de HELLO')
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def accept_tcp6(s, null) :
-    print("OI")
-    while True:
-        conn, addr = s.accept()
-        _thread.start_new_thread(multi_thread, (conn,s))
+        s.bind((HOST,0))
+        port_udp = s.getsockname()[1]
+        message = message + port_udp.to_bytes(4,byteorder='big')
+        conn.sendall(message)
+
+        data = conn.recv(1024)
+
+        sys.exit()
+
+        if int.from_bytes(data[0:2], byteorder='big') == 3 :
+            message = (4).to_bytes(2,byteorder='big')
+            conn.sendall(message)
+
+        while True :
+            data = conn.recv(BUFFER_SIZE)
 
 
-_thread.start_new_thread(accept_tcp4, (s,' '))
-print('Cabo')
+        else :
+            print('[LOG] Erro no recebimento da mensagem de INFO FILE')
+    else :
+        print('[LOG] Erro no recebimento da mensagem de HELLO')
+
+
+while True:
+    conn, addr = s.accept()
+    _thread.start_new_thread(multi_thread, (conn,s))
